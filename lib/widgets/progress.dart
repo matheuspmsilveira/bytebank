18a3831f053f94f11223fad61eaad71dcc082071
Matheus Silveira <matheuspmsilveira@gmail.com>
2022-08-01 00:20:25 -0300

    feat: add progress widget

---
+++ lib/widgets/progress.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Progress extends StatelessWidget {
+  const Progress({Key? key, this.message = 'Loading'}) : super(key: key);
+
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const CircularProgressIndicator(),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Text(message),
+          ),
+        ],
+      ),
+    );
+  }
+}
